d72c404a2417fd0b1bad64ed7f2e87a8a290939d
antonytw <62089961+antonytw@users.noreply.github.com>
2020-03-23 16:01:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function contact() {
+  alert("Not valid");
+  var email = prompt ("What's your email?");
+  if (email != "") {
+    alert ("Thanks. I will reach out to you")
+  }
+  else {
+    alert ("Looks like you changed your mind :)")
+  }
+
+}
